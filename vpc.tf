2340c98b3036e4c7765d8e84a3023da61cd11bd4
Hyacinth Cruz <haiashinsu@gmail.com>
2023-03-01 22:57:51 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -4,13 +4,13 @@ module "network" {
   version = "3.4.0"
   network_name = "haia-network"
   project_id = var.project
-subnets = [
-  {
-    subnet_name   = "haia-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+  subnets = [
+    {
+      subnet_name   = "haia-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
 }
 
 
